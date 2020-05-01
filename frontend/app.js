62b15d3a83e873a928921120222acb659f328475
Jesus <jesussantizo120@gmail.com>
2020-05-01 16:50:12 -0500

    Updated getTasks in controller. Display tasks on index page

--- frontend/app.js
+++ frontend/app.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/tasks', taskRoutes);
 
--- frontend/controllers/task.js
+++ frontend/controllers/task.js
@@ -1,11 +1,11 @@
 const fetch = require('node-fetch');
-const URL = process.env.LOCAL_HOST_API;
+const API_URL = process.env.LOCAL_HOST_API;
 
 exports.getTasks = async (req, res, next) => {
   try {
-    const response = await fetch(`${URL}/tasks`);
-    const data = await response.json();
-    res.render('index', { tasks: data })
+    const response = await fetch(`${API_URL}/tasks`);
+    const { message, tasks } = (await response.json())[0];
+    res.render('index', { tasks: tasks})
   } catch (error) {
     throw Error(error.message);
   }
