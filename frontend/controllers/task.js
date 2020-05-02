6ad95b11ca105b2ccf0f27d4749a09946dbe9892
Jesus <jesussantizo120@gmail.com>
2020-05-01 22:07:01 -0500

    Add form-data module, create form to add new task, send form data to backend server, display newly created task to page

--- frontend/controllers/task.js
+++ frontend/controllers/task.js
@@ -1,3 +1,4 @@
+const FormData = require('form-data');
 const fetch = require('node-fetch');
 const API_URL = process.env.LOCAL_HOST_API;
 
@@ -5,8 +6,28 @@ exports.getTasks = async (req, res, next) => {
   try {
     const response = await fetch(`${API_URL}/tasks`);
     const { message, tasks } = (await response.json())[0];
-    res.render('index', { tasks: tasks})
+    res.render('index', { tasks: tasks })
   } catch (error) {
     throw Error(error.message);
   }
-}
+};
+
+
+exports.createTask = async (req, res, next) => {
+  const { taskName, taskNote } = req.body;
+  const formData = new FormData();
+  formData.append('name', taskName);
+  formData.append('note', taskNote);
+
+  try {
+    await fetch(`${API_URL}/tasks/new-task`, {
+      method: 'POST',
+      body: formData
+    });
+
+    res.redirect('/tasks');
+
+  } catch (error) {
+    throw Error(error.message);
+  }
+};
--- frontend/routes/task.js
+++ frontend/routes/task.js
@@ -3,5 +3,6 @@ const taskController = require('../controllers/task');
 const router = express.Router();
 
 router.get('/', taskController.getTasks);
+router.post('/new-task', taskController.createTask);
 
 module.exports = router;
