ce28ba86f9e17e3d9f8cf68aff72ff804cbab414
Jesus <jesussantizo120@gmail.com>
2020-05-13 15:39:09 -0500

    Add middleware for CORS policy errors

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(multer().none());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+})
+
 app.use('/tasks', taskRoutes);
 
 mongoose.connect(process.env.DB_HOST, {
