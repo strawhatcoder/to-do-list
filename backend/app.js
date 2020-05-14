49beee3c109be0115859568384b41385eff69f83
Jesus <jesussantizo120@gmail.com>
2020-05-14 00:38:30 -0500

    Update CORS middleware to allow POST methods

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(multer().none());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 })
