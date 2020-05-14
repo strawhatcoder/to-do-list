73a3fa26e32973e3f0dbb62f641d31d1011ce04d
Jesus <jesussantizo120@gmail.com>
2020-05-14 01:54:52 -0500

    Update middleware for CORS

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ app.use(multer().none());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
 })
